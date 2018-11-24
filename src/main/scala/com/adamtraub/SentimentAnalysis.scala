82ef05a9ab1b634dce2c4473fd0572310ecf645e
traubad <traubad@vcu.edu>
2018-11-24 16:32:31 -0500

    No more pushing jars

--- src/main/scala/com/adamtraub/SentimentAnalysis.scala
+++ src/main/scala/com/adamtraub/SentimentAnalysis.scala
@@ -22,18 +22,6 @@ import com.google.cloud.language.v1.{Document, LanguageServiceClient}
 import com.google.cloud.language.v1.Document.Type
 import org.apache.flink.streaming.api.scala._
 
-/**
- * Skeleton for a Flink Streaming Job.
- *
- * For a tutorial how to write a Flink streaming application, check the
- * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
 object SentimentAnalysis {
   def main(args: Array[String]) {
 
