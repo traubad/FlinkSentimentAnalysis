3e1e93e7eb61c820c4525237fe4c1da74bca2567
traubad <traubad@vcu.edu>
2018-11-25 18:49:55 -0500

    It's in a good, stable state where it can work well with Sentiment, entities, and categories.  Trying to take it further though

--- src/main/scala/com/adamtraub/SentimentAnalysis.scala
+++ src/main/scala/com/adamtraub/SentimentAnalysis.scala
@@ -21,90 +21,160 @@ package com.adamtraub
 import com.google.cloud.language.v1._
 import com.google.cloud.language.v1.Document.Type
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable.MutableList
+import org.apache.flink.api.java.utils.ParameterTool
+
+import scala.collection.mutable
+
 
 object SentimentAnalysis {
   def main(args: Array[String]) {
 
-      //val url = "localhost"
-      val url = "73.251.32.82"
-      val port = 9001
+    //val url = "localhost"
+    val url = "73.251.32.82"
+    val port = 9001
 
 
-      val env = StreamExecutionEnvironment.getExecutionEnvironment
-      val dataStream = env.socketTextStream(url, port)
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val dataStream = env.socketTextStream(url, port)
 
-      val chatText = dataStream
-        .flatMap { w =>
-          val text = w.split(",")
-            .drop(2)
-            .mkString(",")
+    val parsedStream = dataStream.map { w =>
+      val msg = w.split(",")
+      Message(msg(0), msg(1), msg.drop(2).mkString(","))
+    }
 
-          var outList = new MutableList[TextData]
+    val aggregateStream = parsedStream
+      .keyBy("user", "channel")
+      .timeWindow(Time.seconds(90),Time.seconds(60))
+      .reduce{(Message1, Message2) =>
 
-          val language = LanguageServiceClient.create()
+        if (!Message1.user.equals(Message2.user)) {
+          sys.error("MAJOR PROBLEMS")
+        }
+        Message(
+          channel = Message1.channel,
+          user = Message1.user,
+          text = Message1.text +" "+ Message2.text
+        )
+      }
 
-          val doc = Document.newBuilder()
-            .setContent(text)
-            .setType(Type.PLAIN_TEXT)
-            .build()
+    val sentimentStream = parsedStream
+      .map { mData =>
 
-          val syntax = language.analyzeSyntax(AnalyzeSyntaxRequest.newBuilder()
+        val language = LanguageServiceClient.create()
+        val sentiment = language.analyzeSentiment(Document.newBuilder()
+          .setContent(mData.text)
+          .setType(Type.PLAIN_TEXT)
+          .build()).getDocumentSentiment
+
+        language.close
+
+        MessageSentiment(mData, Sentiment(sentiment.getScore, sentiment.getMagnitude))
+      }
+
+    val sentimentSentenceStream = parsedStream
+      .map { mData =>
+
+        val language = LanguageServiceClient.create()
+        val document = language.analyzeSentiment(Document.newBuilder()
+          .setContent(mData.text)
+          .setType(Type.PLAIN_TEXT)
+          .build())
+        language.close
+
+        document.getSentencesList.map{ sent =>
+          MessageSentiment(
+            message = Message(
+              channel = mData.channel,
+              user = mData.user,
+              text = sent.getText.getContent
+            ),
+            sentiment = Sentiment(sent.getSentiment.getScore, sent.getSentiment.getMagnitude)
+          )
+        }
+      }
+
+    val entityStream = sentimentStream
+      .map { mData =>
+
+        var entities = new MutableList[Entity]
+        val language = LanguageServiceClient.create()
+        val doc = Document.newBuilder()
+          .setContent(mData.message.text)
+          .setType(Type.PLAIN_TEXT)
+          .build()
+
+        language.analyzeEntities(AnalyzeEntitiesRequest
+            .newBuilder()
             .setDocument(doc)
             .setEncodingType(EncodingType.UTF16)
             .build())
-
-          val sentiment = language.analyzeSentiment(doc).getDocumentSentiment
-
-          if(text.split(" ").length >= 25){
-            val classification = language.classifyText(ClassifyTextRequest.newBuilder()
-              .setDocument(doc)
-              .build())
-            for (cat <- classification.getCategoriesList){
-              outList += contentData(cat.getName, cat.getConfidence)
-            }
+          .getEntitiesList
+          .map { ent =>
+            entities += Entity(
+              ent.getName,
+              ent.getSalience,
+              Sentiment(
+                ent.getSentiment.getScore,
+                ent.getSentiment.getMagnitude
+              )
+            )
           }
 
-          outList += sentimentData(text, sentiment.getScore, sentiment.getMagnitude)
+        language.close
+        MessageSentimentEntities(
+          message = mData.message,
+          sentiment = mData.sentiment,
+          entities = entities.toList
+        )
+      }
+
+    val categoryStream = aggregateStream
+      .map { mData =>
+
+        var outList = new mutable.MutableList[Category]
+
+        if(mData.text.split(" ").length >= 25) {
+          val language = LanguageServiceClient.create()
+
+          val doc = Document.newBuilder()
+            .setContent(mData.text)
+            .setType(Type.PLAIN_TEXT)
+            .build()
+
+          language
+            .classifyText(ClassifyTextRequest.newBuilder()
+              .setDocument(doc)
+              .build())
+            .getCategoriesList
+            .map { cat =>
+              outList += Category(cat.getName, cat.getConfidence)
+            }
+          language.close
+        } else {
+          outList += Category("N/A Not enough data", 0)
         }
-      chatText.print()
-      env.execute("Text Analysis")
+        MessageCategories(mData, outList.toList)
+      }
+
+      sentimentSentenceStream.print()
+      entityStream.print()
+      categoryStream.print()
+
+      env.execute("Slack Analysis")
     }
 
-    abstract class TextData {}
-    case class sentimentData(text: String, score: Float, magnitude: Float) extends TextData
-    case class chatData(room: String, user: String, message: String) extends TextData
-    case class contentData(category: String, confidence: Float) extends TextData
+
+    case class Message(channel: String, user: String, text: String)
+    case class Sentiment(score: Float, magnitude: Float)
+    case class MessageSentiment(message: Message, sentiment: Sentiment)
+    case class MessageSentimentEntities(message: Message, sentiment: Sentiment, entities: List[Entity])
+    case class Entity(entity: String, salience: Float, sentiment: Sentiment)
+    case class Annotation()
+
+    case class Category(category: String, confidence: Float)
+    case class MessageCategories(message: Message, categories: List[Category])
 }
-
-
-
-//      val chatText = text
-//        .map { w =>
-//          val split = w.split(",")
-//          if(split.length >= 3) chatData(split(0), split(1), split.drop(2).mkString(","))
-//          else chatData("Unknown", "Unknown", split.mkString(","))
-//        }
-//        .keyBy("room")
-//
-
-//  val  chatMessage = text
-//    .map { w =>
-//      val split = w.split(",")
-//      chatData(split(0), split(1), split.drop(2).mkString(","))
-//    }
-//    .keyBy("room")
-
-
-//    val text = "I fucking hate sharks"
-//    val doc = Document.newBuilder()
-//              .setContent(text)
-//              .setType(Type.PLAIN_TEXT)
-//              .build()
-//    val sentiment = language.analyzeSentiment(doc).getDocumentSentiment()
-//    val x =sentimentData(text, sentiment.getScore(), sentiment.getMagnitude())
-//    println("Text:"+x.text)
-//    println("Score:"+x.score)
-//    println("Magnitude:"+x.magnitude)
